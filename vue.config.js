7c6c53d693e2a2e59bafd65b2d07e013f62c149c
Casper <casper10528@gmail.com>
2022-03-09 10:48:36 +0800

    Removed Git

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    outputDir: 'docs',
+    publicPath: process.env.NODE_ENV === "production" ? "/SHELTER-ZONE" : "/"
+}
+module.exports = {
+  devServer: {
+    disableHostCheck: true
+  }
+};
